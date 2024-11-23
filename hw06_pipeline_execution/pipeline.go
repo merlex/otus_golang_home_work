180804c5091d7d5218b28ecbac9e5eb90a5feae4
Aleksandr Merekin <a.merekin@gmail.com>
2024-11-23 22:14:15 +0300

    home work 6

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,37 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		c := make(Bi)
+		go func() {
+			defer close(c)
+			if done != nil {
+				<-done
+				return
+			}
+		}()
+		return c
+	}
+
+	resultChan := in
+	for _, st := range stages {
+		outStage := make(Bi)
+		go func(stage Stage, chanIn In, chanOut Bi) {
+			defer close(chanOut)
+			currentChanOut := stage(chanIn)
+			for {
+				select {
+				case d, ok := <-currentChanOut:
+					if !ok {
+						return
+					}
+					chanOut <- d
+				case <-done:
+					return
+				}
+			}
+		}(st, resultChan, outStage)
+		resultChan = outStage
+	}
+	return resultChan
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -35,6 +35,7 @@ func TestPipeline(t *testing.T) {
 		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
 		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
 	}
+	emptyStages := []Stage{}
 
 	t.Run("simple case", func(t *testing.T) {
 		in := make(Bi)
@@ -89,5 +90,60 @@ func TestPipeline(t *testing.T) {
 
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
+	})
+
+	t.Run("empty&done case", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		// Abort after 200ms
+		abortDur := sleepPerStage * 2
+		go func() {
+			<-time.After(abortDur)
+			close(done)
+		}()
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, done, emptyStages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Len(t, result, 0)
+		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
+	})
+
+	t.Run("empty case", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, nil, emptyStages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Equal(t, []string{}, result)
+		require.Less(t,
+			int64(elapsed),
+			// ~0.8s for processing 5 values in 4 stages (100ms every) concurrently
+			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
 	})
 }
