7520eef0324c113ec041ff45dd5078878210d436
Aleksandr Merekin <a.merekin@gmail.com>
2024-11-27 10:16:47 +0300

    home work 7

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,71 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrSourcePathEmpty       = errors.New("source path is required")
+	ErrDestinationPathEmpty  = errors.New("destination path is required")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == "" {
+		return ErrSourcePathEmpty
+	}
+	if toPath == "" {
+		return ErrDestinationPathEmpty
+	}
+
+	sourceFileStat, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if !sourceFileStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	if offset < 0 {
+		offset = 0
+	}
+	if offset > sourceFileStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit <= 0 {
+		limit = sourceFileStat.Size()
+	}
+	if limit > sourceFileStat.Size()-offset {
+		limit = sourceFileStat.Size() - offset
+	}
+
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	_, err = sourceFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+	bar := progressbar.Default(limit)
+	proxyReader := progressbar.NewReader(sourceFile, bar)
+	_, err = io.CopyN(destinationFile, &proxyReader, limit)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
